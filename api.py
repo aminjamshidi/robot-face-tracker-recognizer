63f08d00aafd66ef40cb443b0d43f9db97d16c15
unknown <mohamadaminjamshidi73@gmail.com>
2023-01-29 07:18:20 +0330

    finding face with normal framerate,with uploading image

--- api.py
+++ api.py
@@ -1,7 +1,26 @@
 import cv2
+import serial
+import face_recognition as fr
+
+# our libs importing
 from backend.VideoThread import VideoThread
+
+# Qt importing
 from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtCore import QThread
+from PyQt5.QtWidgets import QFileDialog
+
+
+PORT = "COM6"
+MIN_HORIZONTAL_BAND = 0
+MAX_HORIZONTAL_BAND = 180
+MIN_VERTICAL_BAND = 10
+MAX_VERTICAL_BAND = 170
+SIZE_OF_VIEW = (640, 480)
+
+NO_AVALBLE_IMAGE = cv2.imread("icons/NO_AVAILABLE_IMAGE.jpg")
+NO_AVALBLE_IMAGE = cv2.cvtColor(NO_AVALBLE_IMAGE, cv2.COLOR_BGR2RGB)
+NO_AVALBLE_IMAGE = cv2.resize(NO_AVALBLE_IMAGE, (SIZE_OF_VIEW))
 
 
 class api:
@@ -9,8 +28,21 @@ class api:
 
         self.ui_obj = ui_obj
         self.camera = None
+        self.selected_face_encoding = None
+        # self.ser = serial.Serial(PORT, 115200, timeout=1000, parity=serial.PARITY_NONE)
+        self.horizontal_position = 100
+        self.vertical_position = 100
+        str_ = (
+            "m:"
+            + str(self.horizontal_position)
+            + ":"
+            + str(self.vertical_position)
+            + "-"
+        ).encode("ASCII")
+        # self.ser.write(str_)
         self.load_camera()
         self.widget_connector()
+        self.set_image_label(self.ui_obj.LBL_live_camara, NO_AVALBLE_IMAGE)
 
     def load_camera(self):
         """loading camera for captureing video"""
@@ -24,24 +56,35 @@ class api:
     def start_live_on_another_thread(self):
 
         self.thread = QThread()
-        self.worker = VideoThread()
-        self.worker.set_param(camera=self.camera)
-        self.worker.moveToThread(self.thread)
-        self.thread.started.connect(self.worker.video_capture)
-        self.worker.finished.connect(self.thread.quit)
-        self.worker.finished.connect(self.worker.deleteLater)
+        self.Videoworker = VideoThread()
+        self.Videoworker.set_param(
+            camera=self.camera,
+            encoded_wanted_face=self.selected_face_encoding,
+            name_of_encoded_wanted_face="Amin",
+        )
+        self.Videoworker.moveToThread(self.thread)
+        self.thread.started.connect(self.Videoworker.video_capture)
+        self.Videoworker.finished.connect(self.thread.quit)
+        self.Videoworker.finished.connect(self.Videoworker.deleteLater)
         self.thread.finished.connect(self.thread.deleteLater)
-        self.worker.change_frame_signal.connect(self.show_live)
+        self.Videoworker.change_frame_signal.connect(self.show_live)
+        self.Videoworker.x_and_y_pivots_signal.connect(
+            self.guide_camera_to_face_location
+        )
+        self.Videoworker.finished.connect(self.set_no_image_label)
         self.thread.start()
 
     def show_live(self, frame):
+        self.frame = frame
         self.set_image_label(self.ui_obj.LBL_live_camara, frame)
 
     def stop_live(self):
-        self.worker.set_live_flag(value=False)
+        self.Videoworker.set_live_flag(value=False)
+
+    def set_no_image_label(self):
+        self.set_image_label(self.ui_obj.LBL_live_camara, NO_AVALBLE_IMAGE)
 
     def set_image_label(self, label_name, img):
-
         """set imnage in input label"""
         if len(img.shape) != 3:
             img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
@@ -53,7 +96,64 @@ class api:
         )
         label_name.setPixmap(QPixmap.fromImage(convert_to_Qt_format))
 
+    def upload_image(self):
+
+        self.path_of_selected_img = QFileDialog.getOpenFileName(
+            self.ui_obj, "Picture Location", "", "image Files (*.png *.jpg)"
+        )
+        if self.path_of_selected_img:
+            try:
+                selected_facce = fr.load_image_file(str(self.path_of_selected_img[0]))
+                self.selected_face_encoding = fr.face_encodings(selected_facce)[0]
+                self.Videoworker.set_encoded_wanted_face(
+                    self.selected_face_encoding, "amin"
+                )
+                w = self.ui_obj.LBL_selected_image.width()
+                h = self.ui_obj.LBL_selected_image.height()
+                resized_img = cv2.resize(selected_facce, (w, h))
+                self.set_image_label(self.ui_obj.LBL_selected_image, resized_img)
+            except:
+                pass
+        else:
+            pass
+
+    def guide_camera_to_face_location(self, x_y):
+
+        thresh = 15
+        center_x = 640 // 2
+        center_y = 480 // 2
+        face_center_x, face_center_y = x_y
+
+        x_distance = center_x - face_center_x
+        y_distance = center_y - face_center_y
+
+        if abs(x_distance) > thresh:
+            if x_distance > 0:
+                # print("left")
+                self.horizontal_position += 5
+            else:
+                # print("right")
+                self.horizontal_position -= 5
+
+        if abs(y_distance) > thresh:
+            if y_distance > 0:
+                # print("up")
+                self.vertical_position -= 5
+            else:
+                # print("down")
+                self.vertical_position += 5
+
+        str_ = (
+            "m:"
+            + str(self.horizontal_position)
+            + ":"
+            + str(self.vertical_position)
+            + "-"
+        ).encode("ASCII")
+        # self.ser.write(str_)
+
     def widget_connector(self):
         """connect widget to function"""
         self.ui_obj.BTN_startLive.clicked.connect(self.start_live_on_another_thread)
         self.ui_obj.BTN_stopLive.clicked.connect(self.stop_live)
+        self.ui_obj.BTN_upload_photo.clicked.connect(self.upload_image)
--- backend/VideoThread.py
+++ backend/VideoThread.py
@@ -1,27 +1,125 @@
 import cv2
 import numpy as np
+import face_recognition as fr
 from PyQt5.QtCore import pyqtSignal, QThread
+
+import time
 
 
 class VideoThread(QThread):
     finished = pyqtSignal()
     change_frame_signal = pyqtSignal(np.ndarray)
+    x_and_y_pivots_signal = pyqtSignal(tuple)
 
     def set_param(
-        self,
-        camera=None,
+        self, camera=None, encoded_wanted_face=None, name_of_encoded_wanted_face=None
     ):
         self.camera = camera
         self.live_flag = True
+        self.frame = None
+        self.encoded_wanted_face = encoded_wanted_face
+        self.have_target = False
+        self.name_of_encoded_wanted_face = name_of_encoded_wanted_face
+        self.face_locations = None
+        self.face_encodings = None
+
+        self.t1 = time.time()
+        self.t2 = 0
+        self.num_frame_face_encoding = 0
+        self.num_frame_fcae_location = 0
+
+    def set_encoded_wanted_face(self, encoded_wanted_face, name_of_encoded_wanted_face):
+        self.encoded_wanted_face = encoded_wanted_face
+        self.name_of_encoded_wanted_face = name_of_encoded_wanted_face
+        self.have_target = True
 
     def set_live_flag(self, value):
         self.live_flag = value
+
+    def map_instand_for(self, face_encoding, face_location):
+
+        if len(face_encoding) > 0:
+            matches = fr.compare_faces(
+                [self.encoded_wanted_face], face_encoding, tolerance=0.75
+            )
+            top, right, bottom, left = face_location
+            cv2.rectangle(self.frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(
+                self.frame,
+                (left, bottom - 35),
+                (right, bottom),
+                (0, 0, 255),
+                cv2.FILLED,
+            )
+
+            if matches[0]:
+                name = self.name_of_encoded_wanted_face
+                face_center_x = (left + right) / 2
+                face_center_y = (top + bottom) / 2
+                self.x_and_y_pivots_signal.emit((face_center_x, face_center_y))
+            else:
+                name = "Unknown"
+
+            cv2.putText(
+                self.frame,
+                name,
+                (left + 6, bottom - 6),
+                cv2.FONT_HERSHEY_DUPLEX,
+                1.0,
+                (255, 255, 255),
+                1,
+            )
+        return True
+
+    def map_instand_for_simple_version(self, face_location):
+
+        top, right, bottom, left = face_location
+        cv2.rectangle(self.frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+        return True
 
     def video_capture(self):
 
         while self.live_flag:
             ret, frame = self.camera.read()
+
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            self.frame = frame
             if ret:
-                self.change_frame_signal.emit(frame)
+                self.num_frame_face_encoding += 1
+                self.num_frame_fcae_location += 1
+
+                if (self.num_frame_fcae_location % 5) == 1:
+                    self.face_locations = fr.face_locations(
+                        frame, model="hog", number_of_times_to_upsample=1
+                    )
+                    self.num_frame_fcae_location = 0
+
+                if (
+                    ((self.num_frame_face_encoding % 15) == 1)
+                    and self.have_target
+                    and self.face_locations != None
+                ):
+                    self.num_frame_face_encoding = 0
+                    self.face_encodings = fr.face_encodings(
+                        frame, self.face_locations, num_jitters=1, model="small"
+                    )
+                if self.have_target and self.face_encodings != None:
+                    _ = list(
+                        map(
+                            self.map_instand_for,
+                            self.face_encodings,
+                            self.face_locations,
+                        )
+                    )
+                else:
+                    _ = list(
+                        map(
+                            self.map_instand_for_simple_version,
+                            self.face_locations,
+                        )
+                    )
+
+                self.change_frame_signal.emit(self.frame)
+
         self.finished.emit()
